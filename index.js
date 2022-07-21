a31e1b36761f257179a3e1e496e1fad88af6dbf2
OGUGUA54 <nndukaobikwelu@gmail.com>
2022-07-21 13:53:48 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("hello")
+let hello = "psychotic dreams at night be hitting juice wrld"
+console.log(hello)
+let name = "juice wrld's"
+console.log(name)
+let obviously = name + hello 
+console.log(obviously)
+let word = 2+2
+console.log(word)
+let x = 2
+let y = 4
+let addition = x+y
+let subtraction = x-y
+let multiplication= x*y
+let division = x/y
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
+let equals = x==y
+console.log(equals)
+let notequals = x!=y
+console.log(notequals)

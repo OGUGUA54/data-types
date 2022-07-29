3efed26b80bb44d641a3208def2a041c6489985d
OGUGUA54 <nndukaobikwelu@gmail.com>
2022-07-29 11:42:06 +0100

    new commit

---
+++ classwork.js
@@ -0,0 +1,7 @@
+
+if (sunny){ 
+    console.log("carry an umbrella")}
+    if (rainy){ 
+        console.log("carry a rain coat")}
+        if (clearsky){ 
+            console.log("go out and have a nice day!")}

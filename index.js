39145c02226b460af4a7e7b606dd482556c2cce5
OGUGUA54 <nndukaobikwelu@gmail.com>
2022-07-29 10:21:31 +0100

    java script

--- index.js
+++ index.js
@@ -3,21 +3,49 @@ let hello = "psychotic dreams at night be hitting juice wrld"
 console.log(hello)
 let name = "juice wrld's"
 console.log(name)
-let obviously = name + hello 
+let obviously = name + hello
 console.log(obviously)
-let word = 2+2
+let word = 2 + 2
 console.log(word)
 let x = 2
 let y = 4
-let addition = x+y
-let subtraction = x-y
-let multiplication= x*y
-let division = x/y
+let addition = x + y
+let subtraction = x - y
+let multiplication = x * y
+let division = x / y
 console.log(addition)
 console.log(subtraction)
 console.log(multiplication)
 console.log(division)
-let equals = x==y
+let equals = x == y
 console.log(equals)
-let notequals = x!=y
+let notequals = x != y
 console.log(notequals)
+let person = null;
+let emptyArray = [];
+let numbers = [1, 2, 3];
+let randomArray = [1, "Best", true, null];
+let myName = "OGUGUA";
+console.log(myName);
+console.log(numbers);
+console.log(numbers[2]);
+console.log(randomArray[3]);
+console.log(numbers.length);
+console.log(emptyArray.length)
+let newArray = [5, 6, 1, 2, 5, 6, 7, 8, 10, 100, 0, 13]
+console.log(newArray.length)
+let lastPosition = newArray.length - 1;
+console.log(newArray[lastPosition]);
+let newObject 
+let personObject= {
+    name: "ogugua", age: 13,
+    job: "software Engineer",
+    isRich: true,
+};
+console.log(personObject)
+console.log(personObject.name)
+console.log(personObject.job)
+console.log(personObject.age)
+console.log(personObject["name"])
+console.log(personObject["age"])
+console.log(personObject["job"])

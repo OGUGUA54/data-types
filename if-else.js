f37a01091ef394a7383256313f338651535ba6d3
OGUGUA54 <nndukaobikwelu@gmail.com>
2022-07-29 13:59:43 +0100

    new commimt

--- if-else.js
+++ if-else.js
@@ -1,14 +1,1 @@
-let myName = "Ogugua"
-if (myName === "Ogugua") {
-    console.log("yes! i am the one")
-} else { console.log("this is not me") }
-
-
-let weather = "sunny";
-
-if (weather === "sunny") {
-    console.log("carry an umbrella")
-}
-else if (weather === "rainy") {
-    console.log("carry a rain coat")
-} else{ console.log("go out")}
+ for(let count = 1;count<11;count++){}
